377a62db11f1e08a381af2ae5f38bae3f5641a4d
João Rijo <joaovictor_gomes.7@hotmail.com>
2024-10-12 15:20:06 -0300

    criação das models

---
+++ models.py
@@ -0,0 +1,13 @@
+from app import db
+from datetime import datetime
+
+class Vencimento(db.Model):
+    __tablename__ = 'vencimentos'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(50), unique=True, nullable=False)
+    data_vencimento = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __init__(self, user_id, data_vencimento):
+        self.user_id = user_id
+        self.data_vencimento = data_vencimento
